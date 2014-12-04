04483eb5b70a7e3f910c08c0a5c58443a866b29c
Gustav Behm <gustav.behm@gmail.com>
2014-12-04 20:43:46 +0100

    Add ldap connection object and test initialization

--- src/connection.py
+++ src/connection.py
@@ -1,6 +1,10 @@
+import ldap
+
 class Connection:
     """The class managing the LDAP connection."""
 
-    def __init__ (self, url):
+    def __init__ (self, uri):
+        self.uri = uri
+        self.con = ldap.initialize (uri)
         self.connected = False
         pass
--- test/test_connection.py
+++ test/test_connection.py
@@ -1,13 +1,22 @@
 import connection
 import unittest
+import ldap
+import ldap.ldapobject
 
 class BasicConnection(unittest.TestCase):
 
     def setUp (self):
-        self.con = connection.Connection ("ldap://localhost")
+        self.uri = "ldap://localhost"
+        self.con = connection.Connection (self.uri)
+
+    def test_initialization (self):
+        self.assertEqual (self.con.uri, self.uri)
+        self.assertIsInstance (self.con.con, ldap.ldapobject.LDAPObject)
+        self.assertFalse (self.con.connected)
 
     def test_connected (self):
         self.assertTrue (self.con.connected)
+
 
 if __name__ == '__main__':
     unittest.main()
