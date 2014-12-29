6bf9e4dd83d8a79b57506086c4032e2af91c2d1f
Gustav Behm <gustav.behm@gmail.com>
2014-12-29 15:27:27 +0100

    Purge slapd and ldap-utils to (try to) make sure provisioning works

--- test/travis/install-ldap.sh
+++ test/travis/install-ldap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-pwd
+sudo apt-get purge slapd ldap-utils
 sudo DEBIAN_FRONTEND=noninteractive apt-get install slapd ldap-utils
 sudo ldapadd -Y EXTERNAL -H ldapi:/// -f test/travis/ldap_db_init.ldif
 sudo ldapadd -x -D cn=admin,dc=nodomain -w foobar -f test/data.ldif
